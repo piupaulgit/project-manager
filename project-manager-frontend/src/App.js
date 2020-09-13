1f769245795ea5fea0db93b6eba7d84de3bfebf0
piupaulgit <contactpiu619@gmail.com>
2020-09-13 14:38:06 +0530

    material ui added

---
+++ project-manager-frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@material-ui/core/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Button variant="contained" color="primary">
+          <span class="material-icons">lock</span>Hello World
+        </Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
