afd7ef70138327e5ece94489b302af77d6f5bf2a
piupaulgit <contactpiu619@gmail.com>
2020-10-30 08:34:19 +0530

    table added

--- project-manager-frontend/src/pages/projects/ProjectsList.js
+++ project-manager-frontend/src/pages/projects/ProjectsList.js
@@ -14,59 +14,23 @@ const useStyles = makeStyles({
 });
 
 const columns = [
-    { id: 'name', label: 'Name', minWidth: 170 },
-    { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
-    {
-      id: 'population',
-      label: 'Population',
-      minWidth: 170,
-      align: 'right',
-      format: (value) => value.toLocaleString('en-US'),
-    },
-    {
-      id: 'size',
-      label: 'Size\u00a0(km\u00b2)',
-      minWidth: 170,
-      align: 'right',
-      format: (value) => value.toLocaleString('en-US'),
-    },
-    {
-      id: 'density',
-      label: 'Density',
-      minWidth: 170,
-      align: 'right',
-      format: (value) => value.toFixed(2),
-    },
+    { id: 'logo', label: '#', minWidth: 170 },
+    { id: 'name', label: 'Projects', minWidth: 100 },
+    { id: 'endDate', label: 'Due Date', minWidth: 100 },
+    { id: 'status', label: 'Status', minWidth: 100 }
   ];
-  function createData(name, code, population, size) {
-    const density = population / size;
-    return { name, code, population, size, density };
+  function createData(name,endDate,status) {
+    return { name,endDate,status };
   }
   
-  const rows = [
-    createData('India', 'IN', 1324171354, 3287263),
-    createData('China', 'CN', 1403500365, 9596961),
-    createData('Italy', 'IT', 60483973, 301340),
-    createData('United States', 'US', 327167434, 9833520),
-    createData('Canada', 'CA', 37602103, 9984670),
-    createData('Australia', 'AU', 25475400, 7692024),
-    createData('Germany', 'DE', 83019200, 357578),
-    createData('Ireland', 'IE', 4857000, 70273),
-    createData('Mexico', 'MX', 126577691, 1972550),
-    createData('Japan', 'JP', 126317000, 377973),
-    createData('France', 'FR', 67022000, 640679),
-    createData('United Kingdom', 'GB', 67545757, 242495),
-    createData('Russia', 'RU', 146793744, 17098246),
-    createData('Nigeria', 'NG', 200962417, 923768),
-    createData('Brazil', 'BR', 210147125, 8515767),
-  ];
+//   const rows = [];
     
 const ProjectList = () => {
     const classes = useStyles();
     const [projects, setProjects] = useState([]);
     const {user,token} = isAuthenticated();
     const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
     useEffect(() => {
         preLoad();
     },[]);
@@ -82,7 +46,7 @@ const ProjectList = () => {
 
     const preLoad = () => {
         getAllProjects(user._id, token).then(data => {
-            console.log(data,'from js')
+            setProjects(data.data)
         })
     }
     return (
@@ -104,17 +68,21 @@ const ProjectList = () => {
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                        {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                        {projects.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
                             return (
-                                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                                {columns.map((column) => {
-                                    const value = row[column.id];
-                                    return (
-                                    <TableCell key={column.id} align={column.align}>
-                                        {column.format && typeof value === 'number' ? column.format(value) : value}
+                                <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                                    <TableCell>
+                                        {row.id}
                                     </TableCell>
-                                    );
-                                })}
+                                     <TableCell>
+                                        {row.name}
+                                    </TableCell>
+                                     <TableCell>
+                                        {row.endDate}
+                                    </TableCell>
+                                     <TableCell>
+                                        {row.status}
+                                    </TableCell>
                                 </TableRow>
                             );
                             })}
@@ -122,9 +90,9 @@ const ProjectList = () => {
                     </Table>
                 </TableContainer>
                 <TablePagination  
-                rowsPerPageOptions={[10, 25, 100]}
+                rowsPerPageOptions={[5, 25, 100]}
                 component="div"
-                count={rows.length}
+                count={projects.length}
                 rowsPerPage={rowsPerPage}
                 page={page}
                 onChangePage={handleChangePage}
